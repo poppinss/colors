f338ad5b821e1cf7e0af247b21764d9f562394ed
Julien <julien@ripouteau.com>
2022-10-04 11:52:36 +0200

    refactor: update japa

--- test/colors.spec.ts
+++ test/colors.spec.ts
@@ -7,29 +7,29 @@
  * file that was distributed with this source code.
  */
 
-import test from 'japa'
+import { test } from '@japa/runner'
 import { Raw } from '../src/Raw'
 import { Stringify } from '../src/Stringify'
 
 test.group('Colors | Stringify', () => {
-  test('get string representation of color calls', (assert) => {
+  test('get string representation of color calls', ({ assert }) => {
     const colors = new Stringify()
     assert.equal(colors.red('hello world'), 'red(hello world)')
   })
 
-  test('get string representation of nested color calls', (assert) => {
+  test('get string representation of nested color calls', ({ assert }) => {
     const colors = new Stringify()
     assert.equal(colors.red().dim('hello world'), 'red(dim(hello world))')
   })
 })
 
 test.group('Colors | Raw', () => {
-  test('get string representation of color calls', (assert) => {
+  test('get string representation of color calls', ({ assert }) => {
     const colors = new Raw()
     assert.equal(colors.red('hello world'), 'hello world')
   })
 
-  test('get string representation of nested color calls', (assert) => {
+  test('get string representation of nested color calls', ({ assert }) => {
     const colors = new Raw()
     assert.equal(colors.red().dim('hello world'), 'hello world')
   })
